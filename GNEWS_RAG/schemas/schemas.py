e2b3064fa755d6d33b19a14d25ad6800ae89c298
Gane807405 <168809854+Gane807405@users.noreply.github.com>
2024-08-02 11:52:21 +0530

    Update schemas in gan.py

--- GNEWS_RAG/schemas/schemas.py
+++ GNEWS_RAG/schemas/schemas.py
@@ -3,7 +3,7 @@ from pydantic_core.core_schema import FieldValidationInfo
 from typing import Optional
 class GnewsRequestModel(BaseModel):
     query: str
-    rss_or_gnews: str = "ganesh"
+    rss_or_gnews: str = "ganesh_lagumavarapu"
     brand_name: str
     rss_link : Optional[str]=None
     @field_validator('rss_or_gnews')
